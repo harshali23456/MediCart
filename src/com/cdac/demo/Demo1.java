c7d0db3c9e313893bcccd54e2d7fc2b9a7a1f506
Harshali Khushal Patil <151830553+harshali23456@users.noreply.github.com>
2025-11-26 21:55:53 +0530

    Update Demo1.java

--- src/com/cdac/demo/Demo1.java
+++ src/com/cdac/demo/Demo1.java
@@ -5,5 +5,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello Tanvi");
         System.out.println("Hey Harshali!! Let's Begin..");
+         System.out.println("Updated !! Let's Begin..");
+        
     }
 }
